0154d89d132f6b707beea2d2cec7808166cf98d9
Paul Dewey <paul.dewey@outlook.com>
2020-12-05 18:06:55 +0000

    Finishes day 5

---
+++ day5/prob1.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+MAX_ROW = 127
+MAX_COL = 7
+ID_COEFF = 8
+
+seats = []
+file = IO.foreach("prob1.txt") { |line|
+  seats << line.strip unless line.strip.empty?
+}
+
+def binary_search(lo, hi, s)
+  ch = s[0]
+  if ['F', 'L'].include? ch
+    return lo if s.size == 1
+    return binary_search(lo, (lo + hi) / 2, s[1..-1])
+  elsif ['B', 'R'].include? ch
+    return hi if s.size == 1
+    return binary_search(((lo + hi) / 2.to_f).ceil, hi, s[1..-1])
+  end
+end
+
+ids = []
+seats.each do |seat|
+  row = binary_search(0, MAX_ROW, seat[0, 7])
+  col = binary_search(0, MAX_COL, seat[7..-1])
+
+  ids << (ID_COEFF * row) + col
+end
+
+puts "MAX: #{ids.max}"
